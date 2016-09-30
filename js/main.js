2bff228b9bc3707de8709aeb2ea83ce95e3d4f53
Jairo <jairo.coronado@servinformacion.com>
2016-09-29 21:59:27 -0500

    js final dos

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 console.log("Hello world");
-
+/*
 var saludar = function (param) {
   var is_num = Number.isNaN(param);
   if (is_num) {
-    var param = param.toLowerCase();
+  var param = param.toLowerCase();
   }
   if (param=="dia" || param<=12 && param >=0) {
     console.log("Buenos Dias");
@@ -17,8 +17,8 @@ var saludar = function (param) {
   {
     console.log("Buenas");
   }
-}
-
+};
+*/
 var suerte = function (numero){
 
   if (numero === "13") {
@@ -27,9 +27,20 @@ var suerte = function (numero){
     console.log("Buena Suerte");
   }
 
-}
+};
 
 num=0;
 while (num<=10) {
 console.log(++num);
+}
+
+
+var casa = {altura : 200 , ancho : 100};
+
+var casota = Object.create(casa);
+casota.ventanas=100;
+var arreglo =["a" , 5 , true , "abc"];
+for (var prop in arreglo) {
+  console.log("prop ->" + prop);
+  console.log("arreglo[pos]->" + arreglo[prop]);
 }
